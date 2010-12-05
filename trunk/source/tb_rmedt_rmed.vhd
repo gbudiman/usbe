9a8173dd7d1a5637450a68aa337074724fa0ac56
wahyu.g <wahyu.g@5bd0af91-7bd0-11fb-992c-3534ee7e50b2>
2010-12-05 01:49:40 +0000

    Fixed transmitter, added T-R testing

--- trunk/source/tb_rmedt_rmed.vhd
+++ trunk/source/tb_rmedt_rmed.vhd
@@ -151,9 +151,42 @@ procedure STRINGtoNRZI (
   variable D_Last: std_logic;
   variable data: std_logic_vector(7 downto 0);
   begin
-    for i in 0 to length loop
+    count := bc_count;
+    for i in 1 to length loop
       data := CONV_STD_LOGIC_VECTOR(CONV_INTEGER(CHARACTER'POS(word(i))), 8);
+      for i in 0 to 7 loop
+        if (data(i) = '0') then
+          count := 0;
+          D <= not(D);
+          D_MIN <= D;
+        else
+          if (count = 5) then
+            D_Last := D;
+            if (data(i) = '0') then
+              count := 1;
+            else
+              count := 0;
+            end if;
+            D <= not(D);
+            D_MIN <= D;
+            wait for 8*Period;
+            if (data(i) = '0') then
+              D <= not(D_Last);
+              D_MIN <= D_Last;
+            else
+              D <= (D_LAST);
+              D_MIN <= not(D_Last);
+            end if;
+          else
+            count := count + 1;
+            D <= D;
+            D_MIN <= not(D);
+          end if;
+        end if;
+        wait for 8*Period;
+      end loop;
     end loop;
+    bc_count := count;
   end STRINGtoNRZI;
 
 procedure sendEOP (
@@ -230,7 +263,7 @@ process
     RST <= '0';
     wait for 12 ns;
   
-  sendUART(x"21", serial_in); -- !
+    sendUART(x"21", serial_in); -- !
     sendUART(x"21", serial_in); -- !
     sendUART(x"54", serial_in); -- T
     sendUART(x"45", serial_in); -- E
@@ -247,6 +280,18 @@ process
     HEXtoNRZI("10000000", BC, DP1_RX, DM1_RX);
     HEXtoNRZI(x"11", BC, DP1_RX, DM1_RX);
     report "Sending..." severity note;
+    --STRINGtoNRZI("This is a long string", 21, BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"FF", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"FF", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"FF", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"FF", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"FF", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"FF", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"FF", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"FF", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"BE", BC, DP1_RX, DM1_RX);
+    HEXtoNRZI(x"BE", BC, DP1_RX, DM1_RX);
+    sendEOP(0, DP1_RX, DM1_RX);
     --HEXtoNRZI(x"30", BC, DP1_RX, DM1_RX);
 --    HEXtoNRZI(x"31", BC, DP1_RX, DM1_RX);
 --    HEXtoNRZI(x"32", BC, DP1_RX, DM1_RX);
@@ -255,17 +300,17 @@ process
 --    HEXtoNRZI(x"35", BC, DP1_RX, DM1_RX);
 --    HEXtoNRZI(x"36", BC, DP1_RX, DM1_RX);
 --    HEXtoNRZI(x"37", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"9e", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"13", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"cf", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"a6", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"45", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"77", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"8b", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"a4", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"BE", BC, DP1_RX, DM1_RX);
-    HEXtoNRZI(x"BE", BC, DP1_RX, DM1_RX);
-    sendEOP(0, DP1_RX, DM1_RX);
+    --HEXtoNRZI(x"9e", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"13", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"cf", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"a6", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"45", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"77", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"8b", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"a4", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"BE", BC, DP1_RX, DM1_RX);
+--    HEXtoNRZI(x"BE", BC, DP1_RX, DM1_RX);
+--    sendEOP(0, DP1_RX, DM1_RX);
     
     for i in 0 to 32 loop
       cycleNB(NEXT_BYTE, PDATA_READY);
---
+++ trunk/source/tb_t_r.vhd
@@ -0,0 +1,136 @@
+-- $Id: $
+-- File name:   tb_t_r.vhd
+-- Created:     12/4/2010
+-- Author:      Gloria Budiman
+-- Lab Section: 337-02
+-- Version:     1.0  Initial Test Bench
+
+library ieee;
+--library gold_lib;   --UNCOMMENT if you're using a GOLD model
+use ieee.std_logic_1164.all;
+--use gold_lib.all;   --UNCOMMENT if you're using a GOLD model
+
+entity tb_t_r is
+  generic (Period : Time :=  10.4167 ns);
+end tb_t_r;
+
+architecture TEST of tb_t_r is
+
+  function INT_TO_STD_LOGIC( X: INTEGER; NumBits: INTEGER )
+     return STD_LOGIC_VECTOR is
+    variable RES : STD_LOGIC_VECTOR(NumBits-1 downto 0);
+    variable tmp : INTEGER;
+  begin
+    tmp := X;
+    for i in 0 to NumBits-1 loop
+      if (tmp mod 2)=1 then
+        res(i) := '1';
+      else
+        res(i) := '0';
+      end if;
+      tmp := tmp/2;
+    end loop;
+    return res;
+  end;
+
+  component t_r
+    PORT(
+         PRGA_OUT : IN std_logic_vector (7 DOWNTO 0);
+         clk : IN std_logic;
+         p_ready : IN std_logic;
+         prga_opcode : IN std_logic_vector (1 DOWNTO 0);
+         rst : IN std_logic;
+         CRC_ERROR : OUT std_logic;
+         EOP_EXTERNAL : OUT std_logic;
+         NEXT_BYTE : OUT std_logic;
+         OPCODE : OUT std_logic_vector (1 DOWNTO 0);
+         RCV_DATA : OUT std_logic_vector (7 DOWNTO 0);
+         R_ERROR : OUT std_logic
+    );
+  end component;
+
+-- Insert signals Declarations here
+  signal PRGA_OUT : std_logic_vector (7 DOWNTO 0);
+  signal clk : std_logic;
+  signal p_ready : std_logic;
+  signal prga_opcode : std_logic_vector (1 DOWNTO 0);
+  signal rst : std_logic;
+  signal CRC_ERROR : std_logic;
+  signal EOP_EXTERNAL : std_logic;
+  signal NEXT_BYTE : std_logic;
+  signal OPCODE : std_logic_vector (1 DOWNTO 0);
+  signal RCV_DATA : std_logic_vector (7 DOWNTO 0);
+  signal R_ERROR : std_logic;
+
+-- signal <name> : <type>;
+procedure sendTransmitter(
+  constant data: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+  constant opcode: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+  signal PRGA_OUT: OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+  signal PRGA_OPCODE: OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+  signal NEXT_BYTE: IN STD_LOGIC;
+  signal P_READY: OUT STD_LOGIC) IS
+begin
+  if (NEXT_BYTE = '0') THEN
+    wait until NEXT_BYTE = '1';
+  END IF;
+  wait for 8 * period;
+  PRGA_OUT <= data;
+  PRGA_opcode <= opcode;
+  P_READY <= '1';
+  wait for period;
+  P_READY <= '0';
+end sendTransmitter;
+
+begin
+  DUT: t_r port map(
+                PRGA_OUT => PRGA_OUT,
+                clk => clk,
+                p_ready => p_ready,
+                prga_opcode => prga_opcode,
+                rst => rst,
+                CRC_ERROR => CRC_ERROR,
+                EOP_EXTERNAL => EOP_EXTERNAL,
+                NEXT_BYTE => NEXT_BYTE,
+                OPCODE => OPCODE,
+                RCV_DATA => RCV_DATA,
+                R_ERROR => R_ERROR
+                );
+
+--   GOLD: <GOLD_NAME> port map(<put mappings here>);
+autoClock: process
+  BEGIN
+    clk <= '0';
+    wait for period/2;
+    clk <= '1';
+    wait for period/2;
+  END process autoClock;
+  
+process
+
+  begin
+
+-- Insert TEST BENCH Code Here
+    p_ready <= '0';
+    prga_out <= x"00";
+    prga_opcode <= "10";
+    rst <= '1';
+    wait for 5 ns;
+    rst <= '0';
+    wait for 5 ns;
+    
+    sendTransmitter(x"80", "00", PRGA_OUT, PRGA_OPCODE, NEXT_BYTE, P_READY);
+    sendTransmitter(x"FF", "01", PRGA_OUT, PRGA_OPCODE, NEXT_BYTE, P_READY);
+    sendTransmitter(x"FF", "01", PRGA_OUT, PRGA_OPCODE, NEXT_BYTE, P_READY);
+    sendTransmitter(x"FF", "01", PRGA_OUT, PRGA_OPCODE, NEXT_BYTE, P_READY);
+    sendTransmitter(x"FF", "01", PRGA_OUT, PRGA_OPCODE, NEXT_BYTE, P_READY);
+    sendTransmitter(x"FF", "01", PRGA_OUT, PRGA_OPCODE, NEXT_BYTE, P_READY);
+    sendTransmitter(x"FF", "11", PRGA_OUT, PRGA_OPCODE, NEXT_BYTE, P_READY);
+    --PRGA_OUT <= 
+    --clk <= 
+    --p_ready <= 
+    --prga_opcode <= 
+    --rst <= 
+    wait;
+  end process;
+end TEST;
